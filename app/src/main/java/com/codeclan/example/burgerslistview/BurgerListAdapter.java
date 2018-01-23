3f30d2a69ac0691dbfec3231d1aad8f193eaf6f1
Graeme Brown <graemehbrown@googlemail.com>
2018-01-23 23:06:54 +0000

    Added ImageView to burger_item.xml to display an icon in the list.  This is the same image for each row but it could be extended to use multiple images as multiple images could be stored in the imgid array.
    This image view is linked up in the BurgerListAdapter and MainActivity classes.

--- app/src/main/java/com/codeclan/example/burgerslistview/BurgerListAdapter.java
+++ app/src/main/java/com/codeclan/example/burgerslistview/BurgerListAdapter.java
@@ -6,19 +6,21 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by graemebrown on 23/01/2018.
  */
 
 public class BurgerListAdapter extends ArrayAdapter<Burger> {
+    private final Integer[] imgid;
 
-    public BurgerListAdapter(@NonNull Context context, @NonNull ArrayList<Burger> burgers) {
+    public BurgerListAdapter(@NonNull Context context, @NonNull ArrayList<Burger> burgers, Integer[] imgid) {
         super(context, 0, burgers);
+        this.imgid = imgid;
     }
 
     public View getView(int position, View listItemView, ViewGroup parent) {
@@ -28,11 +30,12 @@ public class BurgerListAdapter extends ArrayAdapter<Burger> {
 
         Burger currentBurger = getItem(position);
         TextView burgerName = listItemView.findViewById(R.id.burger_name);
+        ImageView imageView = listItemView.findViewById(R.id.imageView) ;
         burgerName.setText(currentBurger.getName());
 
         TextView burgerPrice = listItemView.findViewById(R.id.burger_price);
         burgerPrice.setText(currentBurger.getPrice().toString());
-
+        imageView.setImageResource(imgid[0]);
         return listItemView;
     }
 }
--- app/src/main/java/com/codeclan/example/burgerslistview/MainActivity.java
+++ app/src/main/java/com/codeclan/example/burgerslistview/MainActivity.java
@@ -4,7 +4,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,13 +11,19 @@ public class MainActivity extends AppCompatActivity {
     private BurgerList burgersList;
     ArrayList<Burger> burgersToDisplay;
 
+
+    Integer[] imgid={
+            R.drawable.burger_small
+
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         burgersList = new BurgerList();
         burgersToDisplay = burgersList.getBurgers();
-        BurgerListAdapter adapter = new BurgerListAdapter(this, burgersToDisplay);
+        BurgerListAdapter adapter = new BurgerListAdapter(this, burgersToDisplay, imgid);
         ListView listView = findViewById(R.id.list_view);
         listView.setAdapter(adapter);
     }
